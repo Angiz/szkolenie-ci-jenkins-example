423c2f99ed3e99bbc8a91af0e68e8f43027a5d05
AnetaGiziewska <aneta.giziewska@netreveal.ai>
2023-09-12 11:52:50 +0200

    [TECH] Setting cron to one minute

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,14 @@
 pipeline {
     agent any
+    triggers {
+        cron('1 * * * *')
+    }
 
     tools {
         // Install the Maven version configured as "M3" and add it to the path.
         maven "maven-3"
         jdk "jdk-17"
     }
-
     stages {
         stage('Clean') {
             steps {
@@ -16,7 +18,7 @@ pipeline {
         stage('Checkout') {
             steps {
                 // Get some code from a GitHub repository
-                git branch: 'main', url: 'https://github.com/spring-projects/spring-petclinic.git'
+                git branch: 'master', url: 'https://github.com/Angiz/Sii_JavaFinal.git'
             }
         }
         stage('Build') {
@@ -27,27 +29,26 @@ pipeline {
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
-
-            post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                success {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                    slackSend (color: 'good', message: 
+        }
+    }
+    post {
+        // If Maven was able to run the tests, even if some of the test
+        // failed, record the test results and archive the jar file.
+        success {
+            junit '**/target/surefire-reports/TEST-*.xml'
+            archiveArtifacts 'target/*.jar'
+            slackSend (color: 'good', message:
                     """Build success!
                     BUILD_TAG: ${BUILD_TAG} 
                     BUILD_URL: ${BUILD_URL}""")
-                }
-                failure {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                    slackSend (color: 'bad', message:
+        }
+        failure {
+            junit '**/target/surefire-reports/TEST-*.xml'
+            archiveArtifacts 'target/*.jar'
+            slackSend (color: 'bad', message:
                     """Build failed!
                     BUILD_TAG: ${BUILD_TAG} 
                     BUILD_URL: ${BUILD_URL}""")
-                }
-            }
         }
     }
 }
